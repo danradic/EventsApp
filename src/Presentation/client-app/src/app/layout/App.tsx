fdca7ebebba5fd37b83697451dbb93e09160346e
danradic <danradic@gmail.com>
2022-12-02 19:47:23 +0100

    add TypeScript interface for type safety example

--- src/Presentation/client-app/src/app/layout/App.tsx
+++ src/Presentation/client-app/src/app/layout/App.tsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios'
 import { Header, List } from 'semantic-ui-react';
+import { Activity } from '../models/activity';
 
 function App() {
-  const [activities, setActivities] = useState([]);
+  const [activities, setActivities] = useState<Activity[]>([]);
 
   useEffect(() => {
     axios.get("http://localhost:5000/api/activities").then(response => {
@@ -11,12 +12,11 @@ function App() {
     })
   }, []);
 
-
   return (
     <div className="">
       <Header as="h2" icon="users" content="EventsApp"/>
         <List>
-          {activities.map((activity: any) => (
+          {activities.map(activity => (
             <List.Item key={activity.id}>
               {activity.title}
             </List.Item>
