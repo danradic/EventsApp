6aa9ba6394ec8f72f61f4d61fa4684ebd36a7001
Dan Radic <dan.radic@lateralinc.onmicrosoft.com>
2022-12-09 23:11:35 +0100

    add initial domain store and store factory

--- src/Presentation/client-app/src/app/layout/App.tsx
+++ src/Presentation/client-app/src/app/layout/App.tsx
@@ -1,12 +1,15 @@
 import { Container } from 'semantic-ui-react';
 import NavBar from './NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
+import { useStore } from '../stores/storeContext';
 
 function App() {
+  const {activityStore} = useStore();
   return (
     <>
       <NavBar />
       <Container style={{marginTop: "6em"}}>
+        <h2>{activityStore.title}</h2>
         <ActivityDashboard />
       </Container>
     </>
