a7d3ceb966fcdfc4e631efe913294cc0781882f2
danradic <danradic@gmail.com>
2022-12-10 20:00:50 +0100

    refactor to mobx: creating an activity

--- src/Presentation/client-app/src/app/stores/activityStore.ts
+++ src/Presentation/client-app/src/app/stores/activityStore.ts
@@ -1,6 +1,7 @@
 import { action, makeAutoObservable, runInAction } from "mobx";
 import activityApiClient from "../api/activityApiClient";
 import { Activity } from "../models/activity";
+import { v4 as uuidv4 } from 'uuid';
 
 export default class ActivityStore {
     activities: Activity[] = [];
@@ -42,6 +43,7 @@ export default class ActivityStore {
 
     cancelSelectedActivity = () => {
         this.selectedActivity = undefined;
+        this.editMode = false;
     }
 
     openForm = (id?: string) => {
@@ -51,5 +53,42 @@ export default class ActivityStore {
 
     closeForm = () => {
         this.editMode = false;
+    }
+
+    createActivity = async (activity: Activity) => {
+        this.loading = true;
+        activity.id = uuidv4();
+        try {
+            await activityApiClient.addActivity(activity);
+            runInAction(() => {
+                this.activities.push(activity);
+                this.selectedActivity = activity;
+                this.editMode = false;
+                this.loading = false;
+            });
+        } catch (error) {
+            console.log(error);
+            runInAction(() => {
+                this.loading = false;
+            });
+        }
+    }
+
+    updateActivity = async (activity: Activity) => {
+        this.loading = true;
+        try {
+            await activityApiClient.updateActivity(activity);
+            runInAction(() => {
+                this.activities = [...this.activities.filter(a => a.id !== activity.id), activity]
+                this.selectedActivity = activity;
+                this.editMode = false;
+                this.loading = false;
+            });
+        } catch (error) {
+            console.log(error);
+            runInAction(() => {
+                this.loading = false;
+            });
+        }
     }
 }
