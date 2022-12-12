206311e3e8b35788c41f734d311045c3df045437
Dan Radic <dan.radic@lateralinc.onmicrosoft.com>
2022-12-12 01:48:25 +0100

    add routes and links to create/list/detail components

--- src/Presentation/client-app/src/app/layout/NavBar.tsx
+++ src/Presentation/client-app/src/app/layout/NavBar.tsx
@@ -1,15 +1,16 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import { Container, Menu } from "semantic-ui-react";
 
 export default function NavBar(){
     return (
         <Menu inverted fixed="top">
             <Container>
-                <Menu.Item header>
+                <Menu.Item as={NavLink} to='/' header>
                     <i className="react icon logo"></i>
                     EventsApp
                 </Menu.Item>
-                <Menu.Item name="Activities"/>
+                <Menu.Item as={NavLink} to='/activities' name="Activities"/>
             </Container>
         </Menu>
     )
--- src/Presentation/client-app/src/features/activities/form/ActivityForm.tsx
+++ src/Presentation/client-app/src/features/activities/form/ActivityForm.tsx
@@ -5,7 +5,7 @@ import { useStore } from "../../../app/stores/storeContext";
 
 export default observer(function ActivityForm() {
     const {activityStore} = useStore();
-    const {selectedActivity, closeForm, createActivity, updateActivity, loading} = activityStore;
+    const {selectedActivity, createActivity, updateActivity, loading} = activityStore;
 
     const initialState = selectedActivity ?? {
         id: '',
@@ -38,7 +38,7 @@ export default observer(function ActivityForm() {
                 <Form.Input placeholder='City' value={activity?.city} name='city' onChange={handleInputChange} />
                 <Form.Input placeholder='Venue' value={activity?.venue} name='venue' onChange={handleInputChange} />
                 <Button loading={loading} floated='right' positive type="submit" content='Submit' name='title' onChange={handleInputChange} />
-                <Button onClick={closeForm} floated='right' type='button' content='Cancel' name='title' onChange={handleInputChange} />
+                <Button floated='right' type='button' content='Cancel' name='title' onChange={handleInputChange} />
             </Form>
         </Segment>
     )
