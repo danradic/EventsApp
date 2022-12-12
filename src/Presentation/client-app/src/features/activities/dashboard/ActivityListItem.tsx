fe37d4171238f57c66ff601e33454a18acf316cf
danradic <danradic@gmail.com>
2022-12-13 00:08:01 +0100

    style the list items

--- src/Presentation/client-app/src/features/activities/dashboard/ActivityListItem.tsx
+++ src/Presentation/client-app/src/features/activities/dashboard/ActivityListItem.tsx
@@ -1,6 +1,6 @@
 import React, { SyntheticEvent, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { Button, Item, Label } from 'semantic-ui-react';
+import { Button, Icon, Item, Label, Segment } from 'semantic-ui-react';
 import { Activity } from '../../../app/models/activity';
 import { useStore } from '../../../app/stores/storeContext';
 
@@ -8,39 +8,52 @@ interface Props {
     activity: Activity
 }
 
-export default function ActivityListItem({activity}: Props) {
+export default function ActivityListItem({ activity }: Props) {
 
-    const {activityStore} = useStore();
-    const {deleteActivity, loading} = activityStore;
+    const { activityStore } = useStore();
+    const { deleteActivity, loading } = activityStore;
 
     const [target, setTarget] = useState('');
 
-    function handleActivityDelete(e:SyntheticEvent<HTMLButtonElement>, id: string) {
+    function handleActivityDelete(e: SyntheticEvent<HTMLButtonElement>, id: string) {
         setTarget(e.currentTarget.id);
         deleteActivity(id);
     }
-    
+
     return (
-        <Item key={activity.id}>
-        <Item.Content>
-            <Item.Header as='a'> {activity.title}</Item.Header>
-            <Item.Meta>{activity.date.toString()}</Item.Meta>
-            <Item.Description>
-                <div>{activity.description}</div>
-                <div>{activity.city}, {activity.venue}</div>
-            </Item.Description>
-            <Item.Extra>
-                <Button as={Link} to={`/activities/${activity.id}`} floated='right' content='View' color='blue' />
-                <Button 
-                    id={activity.id}
-                    loading={loading && target === activity.id} 
-                    onClick={(e) => handleActivityDelete(e, activity.id)} 
-                    floated='right' 
-                    content='DELETE' 
-                    color='red' />
-                <Label basic content={activity.category} />
-            </Item.Extra>
-        </Item.Content>
-    </Item>
+        <Segment.Group>
+            <Segment>
+                <Item.Group>
+                    <Item>
+                        <Item.Image size='tiny' circular src='/assets/user.png' />
+                        <Item.Content>
+                            <Item.Header as={Link} to={`/activites/${activity.id}`}>{activity.title}</Item.Header>
+                            <Item.Description>Hosted by Bob</Item.Description>
+                        </Item.Content>
+                    </Item>
+                </Item.Group>
+            </Segment>
+            <Segment>
+                <span>
+                    <>
+                        <Icon name='clock' /> {activity.date}
+                        <Icon name='marker' /> {activity.venue}
+                    </>
+                </span>
+            </Segment>
+            <Segment secondary>
+                Antendees go here
+            </Segment>
+            <Segment clearing>
+                <span>{activity.description}</span>
+                <Button
+                    as={Link}
+                    to={`/activities/${activity.id}`}
+                    color='teal'
+                    floated='right'
+                    content='View'
+                />
+            </Segment>
+        </Segment.Group>
     )
 }
