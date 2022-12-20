06ad854b36803b67d79192ebafb00a96996329c3
Dan Radic <dan.radic@lateralinc.onmicrosoft.com>
2022-12-20 18:25:32 +0100

    client-app: add NotFound component

--- src/Presentation/client-app/src/app/api/activityApiClient.ts
+++ src/Presentation/client-app/src/app/api/activityApiClient.ts
@@ -1,5 +1,7 @@
-import axios, { AxiosResponse } from "axios";
+import axios, { AxiosError, AxiosResponse } from "axios";
+import { toast } from "react-toastify";
 import { Activity } from "../models/activity";
+import { router } from "../router/Routes";
 
 const sleep = (delay: number) => {
     return new Promise((resolve) => {
@@ -7,27 +9,42 @@ const sleep = (delay: number) => {
     })
 }
 
-const client = axios.create({
-    baseURL: 'http://localhost:5000/api'
-  });
+axios.defaults.baseURL = 'http://localhost:5000/api';
 
-  client.interceptors.response.use(async response => {
-    try {
-        await sleep(700);
-        return response;
-    } catch (error) {
-        console.log(error);
-        return await Promise.reject(error);
+axios.interceptors.response.use(async response => {
+    await sleep(700);
+    return response;
+}, (error: AxiosError) => {
+    const { data, status } = error.response!;
+    switch (status) {
+        case 400:
+            toast.error('bad request');
+            break;
+        case 401:
+            toast.error('unauthorized');
+            break;
+        case 403:
+            toast.error('forbidden');
+            break;
+        case 404:
+            router.navigate('/not-found');
+            break;
+        case 500:
+            toast.error('internal server error');
+            break;
+        default:
+            break;
     }
+    return Promise.reject(error);
 })
 
-const resonseBody = <T> (response: AxiosResponse<T>) => response.data;
+const resonseBody = <T>(response: AxiosResponse<T>) => response.data;
 
 const requests = {
-    get: <T> (url: string) => client.get<T>(url).then(resonseBody),
-    post: <T> (url: string, body: {}) => client.post<T>(url, body).then(resonseBody),
-    put: <T> (url: string, body: {}) => client.put<T>(url, body).then(resonseBody),
-    delete: <T> (url: string) => client.delete<T>(url).then(resonseBody)
+    get: <T>(url: string) => axios.get<T>(url).then(resonseBody),
+    post: <T>(url: string, body: {}) => axios.post<T>(url, body).then(resonseBody),
+    put: <T>(url: string, body: {}) => axios.put<T>(url, body).then(resonseBody),
+    delete: <T>(url: string) => axios.delete<T>(url).then(resonseBody)
 }
 
 const activityApiClient = {
