ca5e9158e17d4afa95d9150aca7ef40d086f59bb
danradic <danradic@gmail.com>
2022-12-21 00:26:28 +0100

    formik with yup usage

--- src/Presentation/client-app/src/features/activities/form/ActivityForm.tsx
+++ src/Presentation/client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,12 +1,13 @@
 import { observer } from "mobx-react-lite";
 import React, { ChangeEvent, useEffect, useState } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
-import { Button, Form, Segment } from "semantic-ui-react";
+import { Button, FormField, Label, Segment } from "semantic-ui-react";
 import LoadingComponent from "../../../app/layout/LoadingComponent";
 import { Activity } from "../../../app/models/activity";
 import { useStore } from "../../../app/stores/storeContext";
 import { v4 as uuidv4 } from 'uuid';
-import { Formik } from "formik";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from 'yup';
 
 export default observer(function ActivityForm() {
     const { activityStore } = useStore();
@@ -26,6 +27,10 @@ export default observer(function ActivityForm() {
             venue: ''
         }
     );
+
+    const validationSchema = Yup.object({
+        title: Yup.string().required('The activity title is required')
+    });
 
     useEffect(() => {
         if (id) loadActivity(id).then(activity => setActivity(activity!))
@@ -49,17 +54,24 @@ export default observer(function ActivityForm() {
 
     return (
         <Segment clearing>
-            <Formik enableReinitialize initialValues={activity} onSubmit={values => console.log(values)}>
-                {({ values: activity, handleChange, handleSubmit }) => (
-                    <Form onSubmit={handleSubmit} autoComplete='off'>
-                        <Form.Input placeholder='Title' value={activity?.title} name='title' onChange={handleChange} />
-                        <Form.TextArea placeholder='Description' value={activity?.description} name='description' onChange={handleChange} />
-                        <Form.Input placeholder='Category' value={activity?.category} name='category' onChange={handleChange} />
-                        <Form.Input type='date' placeholder='Date' value={activity?.date.toString()} name='date' onChange={handleChange} />
-                        <Form.Input placeholder='City' value={activity?.city} name='city' onChange={handleChange} />
-                        <Form.Input placeholder='Venue' value={activity?.venue} name='venue' onChange={handleChange} />
-                        <Button loading={loading} floated='right' positive type="submit" content='Submit' name='title' onChange={handleChange} />
-                        <Button as={Link} to='/activities' floated='right' type='button' content='Cancel' name='title' onChange={handleChange} />
+            <Formik
+                validationSchema={validationSchema}
+                enableReinitialize
+                initialValues={activity}
+                onSubmit={values => console.log(values)}>
+                {({ handleSubmit }) => (
+                    <Form className='ui form' onSubmit={handleSubmit} autoComplete='off'>
+                        <FormField>
+                            <Field placeholder='Title' name='title' />
+                            <ErrorMessage name='title' render={error => <Label basic color='red' content={error} />}/>
+                        </FormField>
+                        <Field placeholder='Description' name='description' />
+                        <Field placeholder='Category' name='category' />
+                        <Field type='date' placeholder='Date' name='date' />
+                        <Field placeholder='City' name='city' />
+                        <Field placeholder='Venue' name='venue' />
+                        <Button loading={loading} floated='right' positive type="submit" content='Submit' name='title' />
+                        <Button as={Link} to='/activities' floated='right' type='button' content='Cancel' name='title' />
                     </Form>
                 )}
             </Formik>
