d117fefa9f9b6b1a65c554c8594a455697bbcd39
Dan Radic <dan.radic@lateralinc.onmicrosoft.com>
2022-12-20 16:11:45 +0100

    client-app: add react-toastify package

--- src/Presentation/client-app/src/app/layout/App.tsx
+++ src/Presentation/client-app/src/app/layout/App.tsx
@@ -1,10 +1,11 @@
-import { Container } from 'semantic-ui-react';
 import NavBar from './NavBar';
 import { Outlet } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 
 function App() {
   return (
     <>
+      <ToastContainer position='bottom-right' hideProgressBar theme='colored' />
       <NavBar />
       <Outlet />
     </>
