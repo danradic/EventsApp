b0e457b2d1800877bdd454cf771baccdecb10464
danradic <danradic@gmail.com>
2022-12-27 14:59:33 +0100

    client app: add login form component

---
+++ src/Presentation/client-app/src/features/users/LoginForm.tsx
@@ -0,0 +1,22 @@
+import { Form, Formik } from "formik";
+import { Button } from "semantic-ui-react";
+import MyTextInput from "../../app/common/form/MyTextInput";
+
+export default function LoginForm() {
+    return (
+        <Formik
+            initialValues={{ email: '', password: '' }}
+            onSubmit={values => console.log(values)}
+        >
+            {({handleSubmit}) => (
+                <Form className="ui form" onSubmit={handleSubmit} autoComplete='off'>
+                    <MyTextInput placeholder='Email' name='email' />
+                    <MyTextInput placeholder='Password' name='password' type='password' />
+                    <Button positive content='Login' type='submit' fluid />
+                </Form>
+            )}
+
+        </Formik>
+
+    )
+}
