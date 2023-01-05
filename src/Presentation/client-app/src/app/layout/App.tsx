da40db81a77877e831f0b3a31532d4be548fedcf
danradic <danradic@gmail.com>
2023-01-05 14:55:41 +0100

    client app: add login persistence

--- src/Presentation/client-app/src/app/layout/App.tsx
+++ src/Presentation/client-app/src/app/layout/App.tsx
@@ -3,10 +3,24 @@ import { Outlet, useLocation } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import { Container } from 'semantic-ui-react';
 import HomePage from '../../features/home/HomePage';
+import { useStore } from '../stores/storeContext';
+import { useEffect } from 'react';
+import LoadingComponent from './LoadingComponent';
 
 function App() {
   const { pathname } = useLocation();
   const isHomePage = pathname === '/';
+  const { commonStore, userStore } = useStore();
+
+  useEffect(() => {
+    if (commonStore.token) {
+      userStore.getUser().finally(() => commonStore.setAppLoaded());
+    } else {
+      commonStore.setAppLoaded();
+    }
+  }, [commonStore, userStore]);
+
+  if(!commonStore.appLoaded) return <LoadingComponent content='Loading app..' />
 
   return (
     <>
--- src/Presentation/client-app/src/app/stores/commonStore.ts
+++ src/Presentation/client-app/src/app/stores/commonStore.ts
@@ -1,21 +1,31 @@
-import { makeAutoObservable } from "mobx";
+import { makeAutoObservable, reaction } from "mobx";
 import { ProblemDetails } from "../models/problemDetails";
 
 export default class CommonStore {
     problemDetails: ProblemDetails | null = null;
-    token: string | null = null;
+    token: string | null = localStorage.getItem('jwt');
     appLoaded = false;
 
     constructor() {
         makeAutoObservable(this);
+
+        reaction(
+            () => this.token,
+            token => {
+                if (token) {
+                    localStorage.setItem('jwt', token)
+                } else {
+                    localStorage.removeItem('jwt')
+                }
+            }
+        )
     }
 
     setServerError(problemDetails: ProblemDetails) {
         this.problemDetails = problemDetails;
     }
-    
+
     setToken = (token: string | null) => {
-        if(token) localStorage.setItem("jwt", token);
         this.token = token;
     }
 
