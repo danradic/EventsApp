71218c1ad77b296c177865b2bf4d6740c6ab3897
Dan Radic <dan.radic@lateralinc.onmicrosoft.com>
2023-01-05 14:55:41 +0100

    client app: add login persistence

--- src/Presentation/client-app/src/app/stores/userStore.ts
+++ src/Presentation/client-app/src/app/stores/userStore.ts
@@ -28,8 +28,16 @@ export default class UserStore {
 
     logout = () => {
         store.commonStore.setToken(null);
-        localStorage.removeItem('jwt');
         this.user = null;
         router.navigate('/');
+    }
+
+    getUser = async () => {
+        try {
+            const user = await apiClient.Account.current();
+            runInAction( () => this.user = user );
+        } catch (error) {
+            console.log(error);
+        }
     }
 }
